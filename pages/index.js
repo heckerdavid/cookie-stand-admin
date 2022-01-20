8f1d2c5851dcb3dd5fea0e53a6d4fd6a6245e8a5
David Hecker <heckerdw@gmail.com>
2022-01-20 13:34:04 -0800

    Adds header and footer.

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,25 @@ export default function Home() {
       <head>
         <title>Cookie Stand Admin</title>
       </head>
-      <h1>hello world</h1>
 
+      <header className="text-3xl p-5 bg-violet-500">
+        <h1>Cookie Stand Admin</h1>
+      </header>
+
+      <CookieStandInput />
+
+      <footer className=" bg-violet-500 mt-1 p-2">
+        <p>&copy; 2022</p>
+      </footer>
+    </>
+  )
+}
+
+function CookieStandInput() {
+  return (
+    <>
+    <h1>coming soon</h1>
+    
     </>
   )
 }
