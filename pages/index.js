b7793f16d6d8f8fde77d11caef7c7190ffac8557
David Hecker <heckerdw@gmail.com>
2022-01-20 16:09:59 -0800

    Moved labels above input box.

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
 
       </main>
 
-      <footer className=" bg-violet-500 mt-1 p-2">
+      <footer className=" bg-violet-500 mt-1 p-4">
         <p>&copy; 2022</p>
       </footer>
     </>
@@ -38,14 +38,25 @@ function CookieStandInput() {
         <input type="text" className="my-auto h-1/2 w-full mr-5" />
       </div>
 
-      <div className="flex py-2">
-        <p className="p-5 my-auto">Min</p>
-        <input type="text" className="flex-auto h-1/2 my-auto"/>
-        <p className="p-5 my-auto">Max</p>
-        <input type="text" className="flex-auto h-1/2 my-auto" />
-        <p className="p-5 my-auto">Avg</p>
-        <input type="text" className="flex-auto h-1/2 my-auto" />
-        <button className="text-lg py-4 px-16 bg-violet-800 m-2 mr-7 rounded">Create</button>
+      <div className="flex py-4">
+
+        <div className="p-3">
+          <p className="p-5 my-auto">Minimum Customers Per Hour</p>
+          <input type="text" className="flex-auto h-1/4 my-auto pl-4 ml-6"/>
+        </div>
+
+        <div className="p-3">
+          <p className="p-5 my-auto">Maximum Customers Per Hour</p>
+          <input type="text" className="flex-auto h-1/4 my-auto pl-4 ml-6" />
+        </div>
+
+        <div className="p-3">
+          <p className="p-5 my-auto">Average Cookies Per Sale</p>
+          <input type="text" className="flex-auto h-1/4 my-auto pl-4 ml-4" />
+        </div>
+
+        <button className="text-lg py-2 px-16 bg-violet-800 m-2 ml-24 rounded">Create</button>
+
       </div>
 
     </form>
