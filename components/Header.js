b02ddbe869c159b10f10a07b37abf999e89ecbea
David Hecker <heckerdw@gmail.com>
2022-01-22 11:29:00 -0800

    Added overview button to Header

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,8 @@
 export default function Header() {
     return (
-        <header className="text-3xl p-5 bg-violet-500">
-          <h1>Cookie Stand Admin</h1>
+        <header className="p-5 bg-violet-500 flex justify-between">
+          <h1 className="text-3xl">Cookie Stand Admin</h1>
+          <a className="bg-gray-400 px-4 py-1 rounded-lg">Overview</a>
         </header>
   )
 }
