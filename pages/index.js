2af360262c09c15bd7d1590db1ecb17ec68a158a
David Hecker <heckerdw@gmail.com>
2022-01-24 15:48:34 -0800

    Added auth.js and used auth hook. Added env variables.

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,23 @@ import axios from "axios"
 import { useState } from "react"
 import CookieStandAdmin from "../components/CookieStandAdmin"
 import LoginForm from "../components/Loginform"
+import { useAuth } from '../contexts/auth'
 
 export default function Home() {
 
-  const [user, setUser] = useState(false)
+  const { user, login } = useAuth()
+
+  const [userName, setUserName] = useState(false)
 
   async function handleLogin(username, password) {
 
-    const response = await axios.post('https://cookie-stand-api-401d17.herokuapp.com/api/token/', {username, password})
-    console.log(response.data)
-    setUser(true)
+    login(username, password)
+    setUserName(username)
   }
 
   return (
     <>
-      { user ? <CookieStandAdmin /> : <LoginForm onLogin={handleLogin}/> }
+      { userName ? <CookieStandAdmin user={userName}/> : <LoginForm onLogin={handleLogin}/> }
     </>
   )
 }
