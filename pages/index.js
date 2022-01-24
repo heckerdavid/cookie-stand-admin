3d9d114b664e71b981616a4a871d00a7676c7756
David Hecker <heckerdw@gmail.com>
2022-01-24 15:15:58 -0800

    Added axios post req to handleLogin.

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
-
+import axios from "axios"
+import { useState } from "react"
 import CookieStandAdmin from "../components/CookieStandAdmin"
 import LoginForm from "../components/Loginform"
 
 export default function Home() {
 
-  const user = true
+  const [user, setUser] = useState(false)
+
+  async function handleLogin(username, password) {
+
+    const response = await axios.post('https://cookie-stand-api-401d17.herokuapp.com/api/token/', {username, password})
+    console.log(response.data)
+    setUser(true)
+  }
 
   return (
     <>
-      { user ? <CookieStandAdmin /> : <LoginForm /> }
+      { user ? <CookieStandAdmin /> : <LoginForm onLogin={handleLogin}/> }
     </>
   )
 }
