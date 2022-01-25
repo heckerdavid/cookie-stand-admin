9b69cef6c7064b85bacd1f8615027315fe86bd6c
David Hecker <heckerdw@gmail.com>
2022-01-24 19:14:52 -0800

    CSS update.

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer({totalLocations}) {
     return (
-        <footer className=" bg-violet-500 mt-1 p-4">
-            <p>{totalLocations} Locations World Wide</p>
+        <footer className=" bg-violet-500 mt-1 p-4 mx-auto">
+            <p className="mx-auto px-96">{totalLocations} Locations World Wide</p>
         </footer>
   )
 }
--- components/Header.js
+++ components/Header.js
@@ -3,13 +3,15 @@ import Link from "next/link";
 
 export default function Header({ user, logout }) {
     return (
-        <header className="p-5 bg-violet-500 flex justify-between">
+        <header className="p-5 bg-violet-500 flex justify-around">
           <h1 className="text-3xl">Cookie Stand Admin</h1>
-          <h2 className="bg-gray-400 px-4 py-1 rounded-lg text-violet-900">{user}</h2>
-          <Link href='/overview'>
-          <a className="bg-gray-400 px-4 py-1 rounded-lg">Overview</a>
-          </Link>
-          <button onClick={logout} className="bg-gray-400 px-4 py-1 rounded-lg">Logout</button>
+          <div className="flex p-2">
+            <h2 className="bg-gray-400 px-4 py-1 rounded-lg text-violet-900">{user}</h2>
+            <Link href='/overview'>
+            <a className="bg-gray-400 px-4 py-1 rounded-lg">Overview</a>
+            </Link>
+            <button onClick={logout} className="bg-gray-400 px-4 py-1 rounded-lg">Logout</button>
+          </div>
         </header>
   )
 }
