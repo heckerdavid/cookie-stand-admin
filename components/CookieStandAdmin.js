90bd9bc4b4e529f3cc24f4ac900f650fa97c11ab
David Hecker <heckerdw@gmail.com>
2022-01-25 14:30:28 -0800

    Header styling update.

--- components/CookieStandAdmin.js
+++ components/CookieStandAdmin.js
@@ -1,7 +1,7 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import CreateForm from "../components/CreateForm"
-import ReportTable from "../components/ReportTable"
+
 import { hours } from '../data'
 import { useState } from 'react'
 import useResource from '../hooks/useResource'
@@ -24,7 +24,7 @@ export default function CookieStandAdmin({ user, logout }) {
           <CreateForm user={user} />
           <div className="my-8 border-violet-900 border-2 p-2 m-4 ml-24">
 
-          <CookieStandTable className="border-violet-900 border-2 p-20" hours={hours}stands={resources || []} deleteStand={deleteResource} />
+          <CookieStandTable className="border-violet-900 border-2 p-20" hours={hours} stands={resources || []} deleteStand={deleteResource} />
           </div>
 
   
