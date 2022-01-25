c06111f3edce06f9ace8b3ad63455831666ecdb9
David Hecker <heckerdw@gmail.com>
2022-01-25 14:05:36 -0800

    refactored table header, moved svg to public folder.

--- components/CookieStandAdmin.js
+++ components/CookieStandAdmin.js
@@ -24,7 +24,7 @@ export default function CookieStandAdmin({ user, logout }) {
           <CreateForm user={user} />
           <div className="my-8 border-violet-900 border-2 p-2 m-4 ml-24">
 
-          <CookieStandTable className="border-violet-900 border-2 p-20" stands={resources || []} deleteStand={deleteResource} />
+          <CookieStandTable className="border-violet-900 border-2 p-20" hours={hours}stands={resources || []} deleteStand={deleteResource} />
           </div>
 
   
--- components/CookieStandTable.js
+++ components/CookieStandTable.js
@@ -1,27 +1,15 @@
+import { delete_icon } from '../public/delete'
 
-export default function CookieStandTable({ stands, deleteStand }) {
+export default function CookieStandTable({ stands, deleteStand, hours }) {
     return (
         <table className="my-8 border-black p-2">
-            <thead>
-                <tr>
-                    <th>Location</th>
-                    <th>6 am</th>
-                    <th>7 am</th>
-                    <th>8 am</th>
-                    <th>9 am</th>
-                    <th>10 am</th>
-                    <th>11 am</th>
-                    <th>12 pm</th>
-                    <th>1 pm</th>
-                    <th>2 pm</th>
-                    <th>3 pm</th>
-                    <th>4 pm</th>
-                    <th>5 pm</th>
-                    <th>6 pm</th>
-                    <th>7 pm</th>
-                    <th>totals</th>
-                </tr>
-            </thead>
+        <thead>
+            <tr>
+                <th className="border border-black">Location</th>
+                {hours.map(hour => <th key={hour} className="border border-black">{hour}</th>)}
+                <th className="border border-black">Totals</th>
+            </tr>
+        </thead>
             <tbody>
                 {stands.map(stand => (
                     <CookieStandRow  key={stand.id} info={stand} deleteStand={deleteStand} />
@@ -33,7 +21,8 @@ export default function CookieStandTable({ stands, deleteStand }) {
 
 function CookieStandRow({ info, deleteStand }) {
 
-    function clickHandler() {
+
+    function handleDelete() {
         deleteStand(info.id);
     }
 
@@ -44,9 +33,7 @@ function CookieStandRow({ info, deleteStand }) {
 
     return (
         <tr>
-            <td>{info.location} <button onClick={clickHandler}><svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-red-600" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
-            </svg></button></td>
+            <td>{info.location} <button onClick={handleDelete}>{delete_icon}</button></td>
             {info.hourly_sales.map((slot, index) => <td key={index}>{slot}</td>)}
             <td>{info.hourly_sales.reduce((num, sum) => num + sum, 0)}</td>
         </tr>
