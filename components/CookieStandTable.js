e41d449b6aac5845113ee53a5d1ce749e70a201f
David Hecker <heckerdw@gmail.com>
2022-01-25 14:23:21 -0800

    Table style update

--- components/CookieStandTable.js
+++ components/CookieStandTable.js
@@ -2,12 +2,12 @@ import { delete_icon } from '../public/delete'
 
 export default function CookieStandTable({ stands, deleteStand, hours }) {
     return (
-        <table className="my-8 border-black p-2">
+        <table className="my-8 border-black p-2 bg-violet-300">
         <thead>
             <tr>
                 <th className="border border-black">Location</th>
-                {hours.map(hour => <th key={hour} className="border border-black">{hour}</th>)}
-                <th className="border border-black">Totals</th>
+                {hours.map(hour => <th key={hour} className="border border-black px-2">{hour}</th>)}
+                <th className="border border-black px-2">Totals</th>
             </tr>
         </thead>
             <tbody>
@@ -32,10 +32,14 @@ function CookieStandRow({ info, deleteStand }) {
     }
 
     return (
-        <tr>
-            <td>{info.location} <button onClick={handleDelete}>{delete_icon}</button></td>
-            {info.hourly_sales.map((slot, index) => <td key={index}>{slot}</td>)}
-            <td>{info.hourly_sales.reduce((num, sum) => num + sum, 0)}</td>
+        <tr className="border border-black">
+
+            <td className="border border-black pl-4">{info.location} <button className="pl-4" onClick={handleDelete}>{delete_icon}</button></td>
+
+            {info.hourly_sales.map((slot, index) => <td className="border border-black px-3" key={index}>{slot}</td>)}
+
+            <td className="border border-black p-1 px-4 ">{info.hourly_sales.reduce((num, sum) => num + sum, 0)}</td>
+
         </tr>
     );
 }
